86f894606f0aba1d8a5f99826eaa5243f8aedbd7
rfortier <rfortier@blackairplane.com>
2025-11-17 13:03:32 +1300

    rr

--- apps/web/modules/shared/composables/useApiCaller.ts
+++ apps/web/modules/shared/composables/useApiCaller.ts
@@ -1,14 +1,18 @@
 import superjson from "superjson";
+import { joinURL } from "ufo";
 import { createTRPCNuxtClient, httpBatchLink } from "trpc-nuxt/client";
 import type { ApiRouter } from "api/modules/trpc";
 
 export const useApiCaller = () => {
   const isProd = import.meta.env.PROD;
-  const trpcBase = isProd ? "/.netlify/functions/server" : "";
+  const runtimeConfig = useRuntimeConfig();
+  const trpcUrl = isProd
+    ? joinURL(runtimeConfig.public.siteUrl, "/.netlify/functions/server/api/trpc")
+    : "/api/trpc";
   const apiCaller = createTRPCNuxtClient<ApiRouter>({
     links: [
       httpBatchLink({
-        url: `${trpcBase}/api/trpc`,
+        url: trpcUrl,
         transformer: superjson,
       }),
     ],
